42692832eadcfee2bc368866c0018477b3dc8bd8
“satoko-k” <satoko617k@gmil.com>
2021-05-10 05:50:50 +0900

    first commit

---
+++ Bicycle.java
@@ -0,0 +1,17 @@
+class Bicycle{
+  
+    private String name;
+    // コンストラクタ
+    Bicycle( String name){
+      this.name=name;
+    }
+    
+    // nameのゲッター
+    public String getName(){
+      return this.name;
+    }
+    
+    
+    
+    
+  }
