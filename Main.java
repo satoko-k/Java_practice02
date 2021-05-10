cffc2390f316aeafec8715e5ddd94ef4ba5e6104
satoko-k <satoko617k@gmil.com>
2021-05-11 05:14:53 +0900

    2nd

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ class Main {
     public static void main(String[] args) {
       Bicycle bicycle = new Bicycle("ビアンキ","みどり");
       System.out.println("【自転車の情報】");
+      
     //   System.out.println("名前：" + bicycle.getName() );
       bicycle.printData();
       System.out.println("-----------------------");
